d6f361783f9480f3bb087de23de59bef21d5f8a4
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-01-02 17:47:39 +0530

    Initial commit

---
+++ app/src/main/java/dev/revington/portfolio/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.revington.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
